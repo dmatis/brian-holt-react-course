a495521fa942de4b05d6b252c75e8b02d449358c
Darren Matis <darren.matis@sap.com>
2021-03-27 11:45:08 -0700

    Configure Typescript

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
 
+const Details = lazy(() => import('./Details'));
+
 const App = () => {
   const theme = useState("darkblue");
+
+
   return (
     <ThemeContext.Provider value={theme}>
       <div>
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>Loading Route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
